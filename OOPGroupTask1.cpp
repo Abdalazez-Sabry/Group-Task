e17e84f7b0f955f401762257825ca4d06f37e7a9
Sabry0 <abdalazezsabry2003@gmail.com>
2022-10-08 14:35:21 +0200

    added string number checker

--- OOPGroupTask1.cpp
+++ OOPGroupTask1.cpp
@@ -3,25 +3,43 @@
 #include <string>
 using namespace std;
 
-
 class BigDecimalInt {
-    public:
-    string classString;
-    vector<string> inputVector;
-    BigDecimalInt(int inputInt){
-        classString = to_string(inputInt);
-    }
-    BigDecimalInt(string inputString){
-        classString = inputString;
-    }
-};
-BigDecimalInt operator+ (BigDecimalInt anotherDec) {
+    private:
+        string number;
 
-}
+    public:
+        BigDecimalInt(int inputInt){
+            number = to_string(inputInt);
+        }
+
+        BigDecimalInt(string inputString){
+            number = inputString;
+            checkStrInput(number);
+        }
+        
+        bool isInteger(string toCheck){
+            if (toCheck.find_first_not_of("-+123456789") != string::npos){
+                return false;
+            }
+            if(toCheck.substr(1).find_first_not_of("123456789") != string::npos){
+                return false;
+            }
+            return true;
+        }
+
+        void checkStrInput(string toCheck){
+            if (isInteger(toCheck)){
+                return;
+            }else{
+                number = "0";
+                cout << "\n\t--The input is not a number--\n";
+            }
+        }
+
+};
+
 
 
 int main() {
-    string input;
-    cin >> input;
-    BigDecimalInt bigInteger(input);
+    BigDecimalInt hello("hello");
 }
